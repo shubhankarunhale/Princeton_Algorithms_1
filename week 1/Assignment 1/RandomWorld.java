e285d6e86969bfa50c7d59c74afcfaca706efbcf
shubhankarunhale <shubhankarunhale@gmail.com>
2023-01-26 15:02:10 +0530

    Added week 1 hello assignment

---
+++ week 1/Assignment 1/RandomWorld.java
@@ -0,0 +1,8 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWorld {
+    
+    
+}
---
+++ week 1/hello/RandomWorld.java
@@ -0,0 +1,25 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWorld {
+    
+ public static void main(String args[])
+ {
+    int i =0;
+    String champion="";
+    while(true)
+    {
+        if(StdIn.isEmpty())
+        break;
+        String myString 
+        =StdIn.readString();
+        i++;
+        if(StdRandom.bernoulli(1.0/(double)i))
+            champion=myString;
+    }
+    
+    System.out.println(champion);
+ }
+ 
+}
